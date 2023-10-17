73e5b01c7d836d7ef3bc0cf28566b89b8531a824
student <student@gnu.ac.in>
2023-10-17 08:52:04 +0530

    update

--- app/src/main/java/com/example/mad_practical_9_21012011103/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012011103/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_practical_9_21012011103
 
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,34 +9,44 @@ import android.view.animation.AnimationUtils
 import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity(),Animation.AnimationListener {
+    lateinit var imageAnimation : AnimationDrawable
     lateinit var logoimage:ImageView
-    lateinit var logoanimation:Animation
+    lateinit var logoanimation : Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
         logoimage=findViewById(R.id.uvpce)
         logoimage.setBackgroundResource(R.drawable.guni_animation_list)
         logoimage.background as AnimationDrawable
         logoanimation=AnimationUtils.loadAnimation(this,R.anim.twin_animation)
         logoanimation.setAnimationListener(this)
     }
+
     override fun onWindowFocusChanged(hasFocus: Boolean) {
-        if (hasFocus){
+        if (hasFocus)
+        {
             logoanimation.start()
+            logoimage.startAnimation(logoanimation)
+        }
+        else
+        {
+            imageAnimation.stop()
         }
         super.onWindowFocusChanged(hasFocus)
     }
 
     override fun onAnimationStart(p0: Animation?) {
-        TODO("Not yet implemented")
+
+
     }
 
     override fun onAnimationEnd(p0: Animation?) {
-        TODO("Not yet implemented")
+        //intent use to open animation
+        Intent(this,MainActivity::class.java).apply { startActivity(this) }
     }
 
     override fun onAnimationRepeat(p0: Animation?) {
-        TODO("Not yet implemented")
+
     }
 }
